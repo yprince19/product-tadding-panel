8d11a5b11cfb512925669dcb2552afd84b8bf06a
yprince19 <prince.yadav@zaamo.co>
2021-12-29 16:34:48 +0530

    del extra files

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
-import products from "./products";
 import state from "./state";
 import get from "./get";
 import post from "./post";
 
 export default combineReducers({
-  products,
   state,
   get,
   post
